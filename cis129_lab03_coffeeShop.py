9c839ce506522e3cb31b719827a19c14969c0dc1
apardeshi13 <159093846+apardeshi13@users.noreply.github.com>
2024-02-16 19:35:49 -0700

    Update cis129_lab03_coffeeShop.py
    Added Water and Candy as an option to be bought. Added lines for calculation. Fixed a minor mistake where variable for coffee was used to calculate the total of muffins.

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -4,10 +4,12 @@ def bill():
     # stored in strings cof and muf
     print("My Coffe and Muffin Shop")
     cof = int(input("Number of coffees bought?"))
+    water = int(input("Number of water bottles bought?")
+    candy = int(input("Number of candies bought?")
     muf = int(input("Number of muffins bought?"))
     # Calculate the total, turn into integer because variable can be string
     # calculate tax and grand total i.e. total with tax
-    total = (cof*5)+(muf*4)
+    total = (cof*5)+(muf*4)+(water*1)+(candy*1)
     total = int(total)
     tax = 0.06*total
     gtotal = round(total + tax, 2)
@@ -15,7 +17,9 @@ def bill():
     # Print the receipt with a summary with cost, quantity and grand total.
     print("My Coffee and Muffin Shop Receipt")
     print(cof, "Coffee at $5 each: $", cof*5)
-    print(muf, "Muffins at $4 each: $", cof*4)
+    print(muf, "Muffins at $4 each: $", muf*4)
+    print(water, "Water at $1 each: $", water*5)
+    print(cof, "Candy at $1 each: $", candy*5)
     print("6% tax: $", total*0.06)
     print("-----------")
     print("Total: $", gtotal)
